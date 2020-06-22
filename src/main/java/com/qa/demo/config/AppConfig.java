21a7dd08bca6e539038a4fc9b268e4de5dcc42b6
Jordan <jordan.harrison@qa.com>
2020-06-22 12:17:17 +0100

    see?

--- src/main/java/com/qa/demo/config/AppConfig.java
+++ src/main/java/com/qa/demo/config/AppConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+
 public class AppConfig {
 	
 	@Bean // managed object - one that's created by Spring
